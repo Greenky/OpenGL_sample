6a6972edefe7d812c52eef73d7a974171cd13025
Valentyn Mazurok <v.mazurok@samsung.com>
2020-01-15 18:01:25 +0200

    Shaders and color manipulations

--- Source.cpp
+++ Source.cpp
@@ -10,18 +10,22 @@
 const GLchar* vertexShaderSource =
 "#version 330 core\n"
 "layout (location = 0) in vec3 position;\n"
+"layout (location = 1) in vec3 color;\n"
+"out vec3 outColor;\n"
 "void main()\n"
 "{\n"
-"gl_Position = vec4(position.x, position.y, position.z, 1.0);\n"
+	"gl_Position = vec4(position, 1.0);\n"
+	"outColor = color;\n"
 "}\0";
 
 // Фрагментний шейдер
 const GLchar* fragmentShaderSource =
 "#version 330 core\n"
 "out vec4 color;\n"
+"in vec3 outColor;\n"
 "void main()\n"
 "{\n"
-"color = vec4(1.0f, 0.5f, 0.2f, 1.0f);\n"
+	"color = vec4(outColor, 1.0f);\n"
 "}\n\0";
 
 int main()
@@ -68,10 +72,10 @@ int main()
 	GLchar infoLog[512];
 
 	GLfloat vertices[] = {
-	 0.0f,  0.5f, 0.0f,  // Верхній правий кут
-	 0.5f, -0.5f, 0.0f,  // Нижній правий кут
-	-0.5f, -0.5f, 0.0f,  // Нижній лівий кут
-	-0.5f,  0.5f, 0.0f   // Верхній лівий кут
+	 0.0f,  0.5f, 0.0f, 1.0f, 0.0f, 0.0f,  // Верхній правий кут
+	 0.5f, -0.5f, 0.0f, 0.0f, 1.0f, 0.0f,  // Нижній правий кут
+	-0.5f, -0.5f, 0.0f, 0.0f, 0.0f, 1.0f,  // Нижній лівий кут
+	-0.5f,  0.5f, 0.0f, 0.3f, 0.3f, 0.3f   // Верхній лівий кут
 	};
 	GLuint indices[] = {
 	0, 1, 2,   // трикутник 1
@@ -82,77 +86,114 @@ int main()
 	// VAO - Vertex Array Object - вершинний масив
 	// EBO - Element Buffer Object - буфер індексів порядку відмальовки вершин
 	GLuint VBO, VAO, EBO;
+
+	// creating VBO
 	glGenBuffers(1, &VBO);
+	// creating EBO
 	glGenBuffers(1, &EBO);
+	// creating VAO
 	glGenVertexArrays(1, &VAO);
 	glBindVertexArray(VAO);
 
+
 	glBindBuffer(GL_ARRAY_BUFFER, VBO);
 	glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
+
 	glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBO);
 	glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indices), indices, GL_STATIC_DRAW);
 
-	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(GLfloat), (GLvoid*)0);
+	
+	// Vertex Shader draw mode
+	// Vertexes
+	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 6 * sizeof(GLfloat), (GLvoid*)0);
 	glEnableVertexAttribArray(0);
+	//Colors
+	glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, 6 * sizeof(GLfloat), (GLvoid*)(3 * sizeof(GLfloat)));
+	glEnableVertexAttribArray(1);
 
 	glBindVertexArray(0);
 
+	// Creating, and compiling vertex shader
 	GLuint vertexShader;
 	vertexShader = glCreateShader(GL_VERTEX_SHADER);
 	glShaderSource(vertexShader, 1, &vertexShaderSource, NULL);
 	glCompileShader(vertexShader);
-
+	
+	// Debug vertex shader code
 	glGetShaderiv(vertexShader, GL_COMPILE_STATUS, &success);
 	if (!success)
 	{
 		std::fill(infoLog, infoLog + sizeof(infoLog), 0);
 		glGetShaderInfoLog(vertexShader, 512, NULL, infoLog);
 		std::cout << "ERROR in Vertex Shader compilation: " << infoLog << std::endl;
+		return -1;
 	}
 
+	// Creating, and compiling fragment shader
 	GLuint fragmentShader;
 	fragmentShader = glCreateShader(GL_FRAGMENT_SHADER);
 	glShaderSource(fragmentShader, 1, &fragmentShaderSource, NULL);
 	glCompileShader(fragmentShader);
+
+	// Debug fragment shader code
 	glGetShaderiv(fragmentShader, GL_COMPILE_STATUS, &success);
 	if (!success)
 	{
 		std::fill(infoLog, infoLog + sizeof(infoLog), 0);
 		glGetShaderInfoLog(vertexShader, 512, NULL, infoLog);
 		std::cout << "ERROR in Fragment Shader compilation: " << infoLog << std::endl;
+		return -1;
 	}
 
+	// Creating shader program and attaching 2 shaders
 	GLuint shaderProgram;
 	shaderProgram = glCreateProgram();
 	glAttachShader(shaderProgram, vertexShader);
 	glAttachShader(shaderProgram, fragmentShader);
 	glLinkProgram(shaderProgram);
+
+	// Debug program compilation
 	glGetProgramiv(shaderProgram, GL_LINK_STATUS, &success);
 	if(!success) {
 		std::fill(infoLog, infoLog + sizeof(infoLog), 0);
 		glGetProgramInfoLog(shaderProgram, 512, NULL, infoLog);
 		std::cout << "ERROR in Shader Program compilation: " << infoLog << std::endl;
+		return -1;
 	}
+	// Deleting shaders
 	glDeleteShader(vertexShader);
 	glDeleteShader(fragmentShader);
 	//
-	//glPolygonMode(GL_FRONT_AND_BACK, GL_LINE);
+
+	//glPolygonMode(GL_FRONT_AND_BACK, GL_LINE); // Whireframe draw mode
+
+	// Game loop
 	while (!glfwWindowShouldClose(window))
 	{
 		glfwPollEvents();
 
-		// Clear the colorbuffer
+		// Clear the colorbuffer - bgcolor
 		glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
 		glClear(GL_COLOR_BUFFER_BIT);
-
+		
+		//Shader transformatin
+		/*
+		GLfloat timeValue = glfwGetTime();
+		GLfloat greenValue = (sin(timeValue) / 2) + 0.5;
+		GLint vertexColorLocation = glGetUniformLocation(shaderProgram, "outColor");
+		*/
 		glUseProgram(shaderProgram);
+		/*
+		glUniform4f(vertexColorLocation, 0.0f, greenValue, 0.0f, 1.0f);
+		*/
+		//Draw image
 		glBindVertexArray(VAO);
 		glDrawElements(GL_TRIANGLES, 3, GL_UNSIGNED_INT, 0);
 		glBindVertexArray(0);
 
 		glfwSwapBuffers(window);
 	}
-
+	// Delete all data
 	glfwTerminate();
 	return 0;
 }
